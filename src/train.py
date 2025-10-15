7b4c5ca261e82cff482ea933a90ee3b322fa52db
Meheruba Hasin <87016852+meherubahasin@users.noreply.github.com>
2025-10-15 14:13:02 +0600

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,14 @@
 
+from tqdm import tqdm
+def train_epoch(loader, model, criterion, optimizer, device):
+    model.train()
+    total_loss = 0
+    for x, y in tqdm(loader):
+        x, y = x.to(device), y.to(device)
+        optimizer.zero_grad()
+        outputs = model(x)
+        loss = criterion(outputs, y)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    return total_loss/len(loader)
